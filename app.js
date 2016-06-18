cd7eeb2ce8349ff04eb90d8e3b2a722124c61f99
Josh Zylstra <zylstrajosh@gmail.com>
2016-06-18 12:15:56 -0700

    everything except https problems, thanks heroku

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ var app = express();
 var cors = require('cors')
 var port = process.env.PORT || 3000;
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://api.openweathermap.org/data/2.5/weather?id=6173331&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-app.use(allowCrossDomain);
+app.use(cors());
 // app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
--- public/index.js
+++ public/index.js
@@ -1,40 +1,31 @@
 $(document).ready(function(){
-//   $('#seattleWeather').click(function(){
-//     // var resultElement = $('#resultDiv');
-//     $.get( "http://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
-//       console.log(data);
-//       console.log(data.weather[0].description);
-//     $('#resultDiv').html(data.weather[0].main);
-//   console.log( "Load was performed." );
-//     });
-// })
   $('#seattleWeather').click(function(){
     // var resultElement = $('#resultDiv');
-    $.get("https://pro.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
+    $.get("http://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
       console.log(data.weather[0].main);
     $('#resultDiv').html((function() {
     var $mainWeather = data.weather[0].main;
     var $temperatureWeather = data.main.temp;
     var $cityWeather = data.name;
   return ("<h2>" + $cityWeather + "</h2><p>Temperature: " + $temperatureWeather + "</p><p>" + $mainWeather + "</p>");
-}));
-  });
+      }));
+    });
   });
   $('#portlandWeather').click(function(){
     // var resultElement = $('#resultDiv');
-    $.get("https://pro.openweathermap.org/data/2.5/weather?id=5746545&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
+    $.get("http://api.openweathermap.org/data/2.5/weather?id=5746545&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
       console.log(data.weather[0].main);
     $('#resultDiv').html((function() {
     var $mainWeather = data.weather[0].main;
     var $temperatureWeather = data.main.temp;
     var $cityWeather = data.name;
   return ("<h2>" + $cityWeather + "</h2><p>Temperature: " + $temperatureWeather + "</p><p>" + $mainWeather + "</p>");
-}));
-  });
+      }));
+    });
   });
   $('#vancouverWeather').click(function(){
     // var resultElement = $('#resultDiv');
-    $.get("https://pro.openweathermap.org/data/2.5/weather?id=6173331&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
+    $.get("http://api.openweathermap.org/data/2.5/weather?id=6173331&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
       console.log(data.weather[0].main);
     $('#resultDiv').html((function() {
     var $mainWeather = data.weather[0].main;

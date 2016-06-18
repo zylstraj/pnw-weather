ed1bba1df32209dfc61543716e28011a93d73a2a
Josh Zylstra <zylstrajosh@gmail.com>
2016-06-18 11:30:49 -0700

    Procfile

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || "https://pnwweather.herokuapp.com/";
 
 // app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function() {
-  console.log('Server listening on port ' + (port || 3000));
+  console.log('Server listening on port ' + (port || "https://pnwweather.herokuapp.com/"));
 });

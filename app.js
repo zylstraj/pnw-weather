e9243bb1f54543c7448876792b80bf9a1152219f
Josh Zylstra <zylstrajosh@gmail.com>
2016-06-30 14:17:08 -0700

    small changes

--- app.js
+++ app.js
@@ -2,13 +2,46 @@
 
 var express = require('express');
 var app = express();
-var cors = require('cors')
+// var cors = require('cors')
 var port = process.env.PORT || 3000;
-
-app.use(cors());
+var bodyParser = require('body-parser')
+var invocation = new XMLHttpRequest();
+var url = "https://api.forecast.io/forecast/f78dd824963251a1a2bc8cd3b18b634b/47.606,-122.335";
+function callOtherDomain() {
+    if(invocation) {
+      invocation.open('GET', url, true);
+      invocation.onreadystatechange = handler;
+      invocation.send();
+    }
+}
+// app.use(cors());
 // app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   next();
+// });
+app.use(express.static(__dirname + '/public'));
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
 app.listen(port, function() {
   console.log('Server listening on port ' + (port || 3000));
 });
+// 'https://api.forecast.io/forecast/f78dd824963251a1a2bc8cd3b18b634b/47.606,-122.335'
--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $('#seattleWeather').click(function(){
     // var resultElement = $('#resultDiv');
-    $.get("http://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1", function( data ) {
-      console.log(data.weather[0].main);
+    $.get("https://api.forecast.io/forecast/f78dd824963251a1a2bc8cd3b18b634b/47.606,-122.335", function( data ) {
+      console.log(data);
     $('#resultDiv').html((function() {
     var $mainWeather = data.weather[0].main;
     var $temperatureWeather = data.main.temp;
@@ -36,3 +36,4 @@ $(document).ready(function(){
     });
   });
 })
+// http://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial&APPID=0547f7cf3264ad65fb0b9df34fdf9cb1
